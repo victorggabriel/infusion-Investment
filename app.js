8cbcdf17efe78aade667ca63494560716071d4c7
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-12-11 02:41:16 -0400

    Implement Jquery button caller

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Hello Victor now you know how to run javascript in your own console. buajajaaj")
+
+$("#subscription-btn").click(function() {
+   alert("Email has to arrive within 1 minute")
+});
